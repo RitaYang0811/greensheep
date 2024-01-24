960106f201b4e46e2f03783fca1d0ba6e875d931
WuWeiTsung <senby298@gmail.com>
2024-01-25 00:45:29 +0800

    fix:scss problem

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import 'bootstrap'
+// import 'bootstrap'
 
 const app = createApp(App)
 
