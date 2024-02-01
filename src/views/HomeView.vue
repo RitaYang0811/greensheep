4fa539e71aaab7eae92a8a9508e36a7a320a6328
WuWeiTsung <senby298@gmail.com>
2024-02-01 17:41:53 +0800

    feat - 在HomeView中新增Home2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="js" name="HomeView">
 import Home1 from '../components/Home1.vue'
+import Home2 from '../components/Home2.vue'
 </script>
 
 <template>
   <Home1></Home1>
+  <Home2></Home2>
 </template>
