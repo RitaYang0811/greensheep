2e780b61e6c7832011653d6f2f5f8e644fc52818
Uli <ru21041555@gmail.com>
2024-02-18 19:16:56 +0800

    feat - 日期選擇器註解

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -1,5 +1,6 @@
-// date 參數需為此格式
-// Tue Apr 30 2024 00:00:00 GMT+0800 (GMT+08:00)
+// 傳入: Tue Apr 30 2024 00:00:00 GMT+0800 (GMT+08:00)
+// 回傳: 2024-04-30
+
 export function dateFormat(date) {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
--- src/utils/unixToDate.js
+++ src/utils/unixToDate.js
@@ -1,6 +1,8 @@
 import { dateFormat } from "./dateFormat.js";
 
-// unix 參數需為以毫秒為單位 (13 位數)
+// 傳入: 1708185600000 *unix 參數需為以毫秒為單位 (13 位數)
+// 回傳: 2024-02-18
+
 export function unixToDate(unix) {
   const date = new Date(unix);
   const dateFormatted = dateFormat(date)
