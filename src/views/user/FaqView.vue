e425299aacedc791b317fb926c67f9b3dccc1ac7
Uli <ru21041555@gmail.com>
2024-03-05 00:15:32 +0800

    feat - 替換 FAQ Swiper 圖片

--- src/views/user/FaqView.vue
+++ src/views/user/FaqView.vue
@@ -1,13 +1,13 @@
 <template>
   <SwiperImages>
     <template #firstImage>
-      <img src="https://images.unsplash.com/photo-1498075702571-ecb018f3752d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2078&q=80" alt="swiperImage">
+      <img src="@/assets/images/IMG_5672.jpg" alt="swiperImage">
     </template>
     <template #secondImage>
-      <img src="https://images.unsplash.com/photo-1678929459787-b2e064262898?q=80&w=2013&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="swiperImage">
+      <img src="https://images.unsplash.com/photo-1653903414925-adfc47c69b3c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="swiperImage">
     </template>
     <template #thirdImage>
-      <img src="https://images.unsplash.com/photo-1505582866941-6788e0205dd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="swiperImage">
+      <img src="https://images.unsplash.com/photo-1550368566-f9cc32d7392d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="swiperImage">
     </template>
   </SwiperImages>
   <div class="container py-7 py-lg-20" style="min-height: 680px;">
