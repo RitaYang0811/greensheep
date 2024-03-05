4ce9765cd3085596a33b5d1547156bed5a5cac01
Uli <ru21041555@gmail.com>
2024-03-05 14:19:19 +0800

    feat - 前台文章內頁

--- src/views/user/ArticleView.vue
+++ src/views/user/ArticleView.vue
@@ -1,6 +1,30 @@
 <template>
-    <h2>專欄文章詳細頁</h2>
-    {{ article }}
+    <!-- {{ article }} -->
+  <div class="container text-start py-7 py-lg-20">
+    <nav aria-label="breadcrumb ">
+      <ol class="breadcrumb mb-20">
+        <li class="breadcrumb-item"><RouterLink to="/">首頁</RouterLink></li>
+        <li class="breadcrumb-item">
+          <RouterLink to="/articles">專欄文章</RouterLink>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+          {{ article.title }}
+        </li>
+      </ol>
+    </nav>
+
+    <h1 class="h2 text-primary text-center mb-12 fw-bold">
+      {{ article.title }}
+    </h1>
+    <div class="row d-flex d-lg-block mb-20">
+      <div class="col-lg-5 me-4 mb-2" style="float: left;"><img :src="article.image" alt=""></div>
+      <div class="col-12 lh-lg text-primary">
+        <div v-html="article.content"></div>
+      </div>
+    </div>
+
+    <h2 class="h4 text-primary text-center mb-10 fw-bold">猜你也喜歡</h2>
+  </div>
     <VueLoading :active="isLoading" />
 </template>
 <script>
