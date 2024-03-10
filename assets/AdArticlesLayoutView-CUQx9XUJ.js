249a374d3848a44c2542fe01ea9f14dbdbd1f84d
jimkao <Jim87as0821@gmail.com>
2024-03-10 12:15:18 +0800

    Updates

--- assets/AdArticlesLayoutView-CUQx9XUJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,b as s,o as r,j as c}from"./index-BUi18zZB.js";const a={data(){return{}},methods:{}},n={class:"px-lg-10 pt-3 pt-lg-10 text-start"};function p(_,d,i,l,u,m){const t=c("RouterView");return r(),o("div",n,[s(t)])}const x=e(a,[["render",p]]);export{x as default};
