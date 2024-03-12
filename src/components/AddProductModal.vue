70124a23baa3721ac38305be81f61e668c700e99
RitaYang <ritayang0811@gmail.com>
2024-03-12 18:02:55 +0800

    fix - 修改建立商品

--- src/components/AddProductModal.vue
+++ src/components/AddProductModal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="modal modal-xl" tabindex="-1" ref="addModal">
     <div class="modal-dialog modal-dialog-scrollable">
-      <VForm class="modal-content" v-slot="{ errors }" @click="clickUpdate">
+      <VForm class="modal-content" v-slot="{ errors }">
         <div class="modal-header bg-primary">
           <h5 v-if="isNew === true" class="modal-title fw-medium text-white ps-3 py-2">新增商品</h5>
           <h5 v-else class="modal-title fw-medium text-white ps-3 py-2">編輯商品</h5>
@@ -379,10 +379,22 @@
             離開編輯
           </button>
 
-          <button v-if="isNew === true" type="submit" class="btn btn-deco float-end">
+          <button
+            v-if="isNew === true"
+            type="button"
+            class="btn btn-deco float-end"
+            @click="$emit('confirmUpdate', isNew)"
+          >
             新增儲存
           </button>
-          <button v-else type="submit" class="btn btn-deco float-end">修改儲存</button>
+          <button
+            v-else
+            type="button"
+            class="btn btn-deco float-end"
+            @click="$emit('confirmUpdate', isNew)"
+          >
+            修改儲存
+          </button>
           <!-- <button
             v-else-if="tempProduct.is_enabled == 0"
             type="button"
@@ -509,10 +521,10 @@ export default {
         .catch((err) => {
           console.log(err)
         })
-    },
-    clickUpdate() {
-      this.$emit('confirmUpdate', this.isNew)
     }
+    // clickUpdate() {
+    //   this.$emit('confirmUpdate', this.isNew)
+    // }
   },
   watch: {
     tempProduct() {
