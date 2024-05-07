6c386d45c578487f2ac72404f0c532ceb81df984
jimkao <Jim87as0821@gmail.com>
2024-05-08 01:29:00 +0800

    Updates

---
+++ assets/copyTextStore-DndwyxM7.js
@@ -0,0 +1,1 @@
+import{y as r}from"./index-CpizAgXM.js";import{a as e,t as a}from"./sweetalertToast-CfxUk4Rv.js";const p=r("copyTextStore",{actions:{copyTextMethod(o,t){navigator.clipboard.writeText(o).then(()=>{e(`已複製${t}`)}).catch(c=>{a(`無法複製${t}: `)})}}});export{p as c};
